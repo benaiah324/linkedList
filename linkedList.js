9e125adcb99ee582cbe63723df3c3fdec3df99d7
Benaiah Enebeli <258831278+benaiah324@users.noreply.github.com>
2026-04-08 06:44:58 +0100

    Added methods prepend size head tail at

--- linkedList.js
+++ linkedList.js
@@ -25,6 +25,58 @@ class linkedList {
         this.size++;
     }
 
+    prepend(value) {
+        const node = new Node(value);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+
+        this.size++;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    getHead() {
+        if (this.size === 0) {
+            return undefined;
+        }
+        return this.head;
+    }
+
+    getTail() {
+        if (this.size === 0) {
+            return undefined;
+        }
+        let current = this.head;
+
+        while (current.next) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    getNodeAt(index) {
+        if (index < 0 || index >= this.size) {
+            return undefined;
+        }
+        let current = this.head;
+        let count = 0;
+
+        while (count < index) {
+            current = current.next;
+            count++;
+        }
+
+        return current;
+    }
+
     display() {
         let currNode = this.head;
         console.log(currNode.value);
--- linkedList.spec.js
+++ linkedList.spec.js
@@ -5,16 +5,42 @@ describe("linkedList", () => {
 
     beforeEach(() => {
         list = new linkedList();
-    });
-
-    test("should add a node to the end of the list", () => {
         list.append("Shemai");
         list.append("Ruth");
         list.append("Jason");
+    });
 
+    test("Test 1: Should add a node to the end of the list", () => {
         expect(list.head.value).toBe("Shemai");
         expect(list.head.next.value).toBe("Ruth");
         expect(list.head.next.next.value).toBe("Jason");
         expect(list.size).toBe(3);
+    });
+
+    test("Test 2: Should add a node to the beginning of the list", () => {
+        list.prepend("Sam");
+        expect(list.head.value).toBe("Sam");
+        expect(list.head.next.value).toBe("Shemai");
+        expect(list.head.next.next.value).toBe("Ruth");
+        expect(list.head.next.next.next.value).toBe("Jason");
+        expect(list.size).toBe(4);
+    });
+
+    test("Test 3: Should return the size of the list", () => {
+        expect(list.getSize()).toBe(3);
+    });
+
+    test("Test 4: Should return the value of the first node", () => {
+        expect(list.getHead().value).toBe("Shemai");
+    });
+
+    test("Test 5: Should return the value of the last node", () => {
+        expect(list.getTail().value).toBe("Jason");
+    });
+
+    test("Test 6: Should return the value of the node at the given index in the list", () => {
+        expect(list.getNodeAt(0).value).toBe("Shemai");
+        expect(list.getNodeAt(1).value).toBe("Ruth");
+        expect(list.getNodeAt(2).value).toBe("Jason");
     });
 });
